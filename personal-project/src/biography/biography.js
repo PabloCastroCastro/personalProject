3e6f080149ba34ca628c501ac7dab6935fd7553b
Pablo Castro <pablo.castro.pichin@gmail.com>
2019-08-16 10:19:46 +0200

    Biography

--- personal-project/src/biography/biography.js
+++ personal-project/src/biography/biography.js
@@ -1,33 +1,25 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './biography.css'
+import Image from 'react-bootstrap/Image';
+import './biography.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import pablo1 from '../images/pablo1.jpg';
 
 class Biography extends React.Component {
 
     render() {
         return (
             <Container fluid = 'true' id='biography-container-general'>
-                <Row id="youngPablo">
-                    <Col id="youngPabloLeft">
-                        
-                    </Col>
-                    <Col id="youngPabloRight">
-                        <Row id="textYoungPabloRight">
-                            <h1>Lorem ipsum</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
-                            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
-                            <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
-                            <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,</p>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row id="oldPablo">
-                    <Col id="oldPabloLeft"></Col>
-                    <Col id="oldPabloRight"></Col>
-                </Row>
+                <div className = 'text-center' id='youngPabloImg'>
+                    <Image src={pablo1} roundedCircle fluid/>
+                </div>
+                <div id='youngPabloText'>
+                    <h1>Lorem ipsum</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+                    <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+                    <p>Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.</p>
+                    <p>Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,</p>
+                </div>
             </Container>
         );
     }
